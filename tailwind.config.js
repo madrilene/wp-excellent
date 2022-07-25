e849c5818f0610b4ca8d38c9426832ff365f4ace
madrilene <hola@lenesaile.com>
2022-07-25 14:35:45 +0200

    tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
-const plugin = require('tailwindcss/plugin');
-const postcss = require('postcss');
-const postcssJs = require('postcss-js');
+const plugin = require("tailwindcss/plugin");
+const postcss = require("postcss");
+const postcssJs = require("postcss-js");
 
-const clampGenerator = require('./_resources/css-utils/clamp-generator.js');
-const tokensToTailwind = require('./_resources/css-utils/tokens-to-tailwind.js');
+const clampGenerator = require("./_resources/css-utils/clamp-generator.js");
+const tokensToTailwind = require("./_resources/css-utils/tokens-to-tailwind.js");
 
 // Raw design tokens
-const colorTokens = require('./_resources/design-tokens/colors.json');
-const fontTokens = require('./_resources/design-tokens/fonts.json');
-const spacingTokens = require('./_resources/design-tokens/spacing.json');
-const textSizeTokens = require('./_resources/design-tokens/text-sizes.json');
+const colorTokens = require("./_resources/design-tokens/colors.json");
+const fontTokens = require("./_resources/design-tokens/fonts.json");
+const spacingTokens = require("./_resources/design-tokens/spacing.json");
+const textSizeTokens = require("./_resources/design-tokens/text-sizes.json");
 
 // Process design tokens
 const colors = tokensToTailwind(colorTokens.items);
@@ -18,12 +18,12 @@ const fontSize = tokensToTailwind(clampGenerator(textSizeTokens.items));
 const spacing = tokensToTailwind(clampGenerator(spacingTokens.items));
 
 module.exports = {
-  content: ['./src/**/*.{html,js,jsx,mdx,njk,twig,vue}'],
+  content: ["./*.{html,js,php,twig}"],
   presets: [],
   theme: {
     screens: {
-      md: '50em',
-      lg: '80em'
+      md: "50em",
+      lg: "80em",
     },
     colors,
     spacing,
@@ -32,93 +32,93 @@ module.exports = {
     fontWeight: {
       normal: 400,
       bold: 700,
-      black: 800
+      black: 800,
     },
-    backgroundColor: ({theme}) => theme('colors'),
-    textColor: ({theme}) => theme('colors'),
-    margin: ({theme}) => ({
-      auto: 'auto',
-      ...theme('spacing')
+    backgroundColor: ({ theme }) => theme("colors"),
+    textColor: ({ theme }) => theme("colors"),
+    margin: ({ theme }) => ({
+      auto: "auto",
+      ...theme("spacing"),
     }),
-    padding: ({theme}) => theme('spacing')
+    padding: ({ theme }) => theme("spacing"),
   },
   variantOrder: [
-    'first',
-    'last',
-    'odd',
-    'even',
-    'visited',
-    'checked',
-    'empty',
-    'read-only',
-    'group-hover',
-    'group-focus',
-    'focus-within',
-    'hover',
-    'focus',
-    'focus-visible',
-    'active',
-    'disabled'
+    "first",
+    "last",
+    "odd",
+    "even",
+    "visited",
+    "checked",
+    "empty",
+    "read-only",
+    "group-hover",
+    "group-focus",
+    "focus-within",
+    "hover",
+    "focus",
+    "focus-visible",
+    "active",
+    "disabled",
   ],
 
   // Disables Tailwind's reset etc
   corePlugins: {
-    preflight: false
+    preflight: false,
   },
   plugins: [
     // Generates custom property values from tailwind config
-    plugin(function ({addComponents, config}) {
-      let result = '';
+    plugin(function ({ addComponents, config }) {
+      let result = "";
 
       const currentConfig = config();
 
       const groups = [
-        {key: 'colors', prefix: 'color'},
-        {key: 'spacing', prefix: 'space'},
-        {key: 'fontSize', prefix: 'size'},
-        {key: 'fontFamily', prefix: 'font'}
+        { key: "colors", prefix: "color" },
+        { key: "spacing", prefix: "space" },
+        { key: "fontSize", prefix: "size" },
+        { key: "fontFamily", prefix: "font" },
       ];
 
-      groups.forEach(({key, prefix}) => {
+      groups.forEach(({ key, prefix }) => {
         const group = currentConfig.theme[key];
 
         if (!group) {
           return;
         }
 
-        Object.keys(group).forEach(key => {
+        Object.keys(group).forEach((key) => {
           result += `--${prefix}-${key}: ${group[key]};`;
         });
       });
 
       addComponents({
-        ':root': postcssJs.objectify(postcss.parse(result))
+        ":root": postcssJs.objectify(postcss.parse(result)),
       });
     }),
 
     // Generates custom utility classes
-    plugin(function ({addUtilities, config}) {
+    plugin(function ({ addUtilities, config }) {
       const currentConfig = config();
       const customUtilities = [
-        {key: 'spacing', prefix: 'flow-space', property: '--flow-space'},
-        {key: 'colors', prefix: 'spot-color', property: '--spot-color'}
+        { key: "spacing", prefix: "flow-space", property: "--flow-space" },
+        { key: "colors", prefix: "spot-color", property: "--spot-color" },
       ];
 
-      customUtilities.forEach(({key, prefix, property}) => {
+      customUtilities.forEach(({ key, prefix, property }) => {
         const group = currentConfig.theme[key];
 
         if (!group) {
           return;
         }
 
-        Object.keys(group).forEach(key => {
+        Object.keys(group).forEach((key) => {
           addUtilities({
             [`.${prefix}-${key}`]: postcssJs.objectify(
               postcss.parse(`${property}: ${group[key]}`)
-            )
+            ),
           });
         });
       });
-    })
-  ]
+    }),
+  ],
 };
