826f1b7e66f65dce171f2d2da4d08646d0c969f9
madrilene <hola@lenesaile.com>
2024-04-08 15:21:06 +0200

    add flow

--- template-parts/content/content-none.php
+++ template-parts/content/content-none.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying a message that posts cannot be found
  *
@@ -9,54 +10,54 @@
 
 ?>
 
-<section class="no-results not-found">
-	<header class="page-header alignwide">
-		<?php if ( is_search() ) : ?>
+<section class="no-results not-found flow">
 
-			<h1 class="page-title">
-				<?php
-				printf(
-					/* translators: %s: Search term. */
-					esc_html__( 'Results for "%s"', 'twentytwentyone' ),
-					'<span class="page-description search-term">' . esc_html( get_search_query() ) . '</span>'
-				);
-				?>
-			</h1>
+	<?php if (is_search()) : ?>
 
-		<?php else : ?>
+		<h1 class="page-title">
+			<?php
+			printf(
+				/* translators: %s: Search term. */
+				esc_html__('Results for "%s"', 'twentytwentyone'),
+				'<span class="page-description search-term">' . esc_html(get_search_query()) . '</span>'
+			);
+			?>
+		</h1>
 
-			<h1 class="page-title"><?php esc_html_e( 'Nothing here', 'twentytwentyone' ); ?></h1>
+	<?php else : ?>
 
-		<?php endif; ?>
-	</header><!-- .page-header -->
+		<h1 class="page-title"><?php esc_html_e('Nothing here', 'twentytwentyone'); ?></h1>
+
+	<?php endif; ?>
+
 
 	<div class="page-content default-max-width">
 
-		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+		<?php if (is_home() && current_user_can('publish_posts')) : ?>
 
 			<?php
 			printf(
 				'<p>' . wp_kses(
 					/* translators: %s: Link to WP admin new post page. */
-					__( 'Ready to publish your first post? <a href="%s">Get started here</a>.', 'twentytwentyone' ),
+					__('Ready to publish your first post? <a href="%s">Get started here</a>.', 'twentytwentyone'),
 					array(
 						'a' => array(
 							'href' => array(),
 						),
 					)
 				) . '</p>',
-				esc_url( admin_url( 'post-new.php' ) )
+				esc_url(admin_url('post-new.php'))
 			);
 			?>
 
-		<?php elseif ( is_search() ) : ?>
+		<?php elseif (is_search()) : ?>
 
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'twentytwentyone' ); ?></p>
+			<p><?php esc_html_e('Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'twentytwentyone'); ?></p>
 			<?php get_search_form(); ?>
 
 		<?php else : ?>
 
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentytwentyone' ); ?></p>
+			<p><?php esc_html_e('It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentytwentyone'); ?></p>
 			<?php get_search_form(); ?>
 
 		<?php endif; ?>
