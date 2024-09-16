7bcd3cf3e63f3b3d5b66b5237684ab9051303440
madrilene <hola@lenesaile.com>
2024-09-16 10:55:28 +0200

    post pagination

--- core/inc/template-tags.php
+++ core/inc/template-tags.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
  * Custom template tags for this theme
  *
  * @package wp-excellent
  */
 
-if ( ! function_exists( 'twenty_twenty_one_posted_on' ) ) {
+if (! function_exists('twenty_twenty_one_posted_on')) {
 	/**
 	 * Prints HTML with meta information for the current post-date/time.
 	 *
@@ -13,25 +14,26 @@ if ( ! function_exists( 'twenty_twenty_one_posted_on' ) ) {
 	 *
 	 * @return void
 	 */
-	function twenty_twenty_one_posted_on() {
+	function twenty_twenty_one_posted_on()
+	{
 		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 
 		$time_string = sprintf(
 			$time_string,
-			esc_attr( get_the_date( DATE_W3C ) ),
-			esc_html( get_the_date() )
+			esc_attr(get_the_date(DATE_W3C)),
+			esc_html(get_the_date())
 		);
 		echo '<span class="posted-on">';
 		printf(
 			/* translators: %s: Publish date. */
-			esc_html__( 'Published %s', 'twentytwentyone' ),
+			esc_html__('Published %s', 'twentytwentyone'),
 			$time_string // phpcs:ignore WordPress.Security.EscapeOutput
 		);
 		echo '</span>';
 	}
 }
 
-if ( ! function_exists( 'twenty_twenty_one_posted_by' ) ) {
+if (! function_exists('twenty_twenty_one_posted_by')) {
 	/**
 	 * Prints HTML with meta information about theme author.
 	 *
@@ -39,20 +41,21 @@ if ( ! function_exists( 'twenty_twenty_one_posted_by' ) ) {
 	 *
 	 * @return void
 	 */
-	function twenty_twenty_one_posted_by() {
-		if ( ! get_the_author_meta( 'description' ) && post_type_supports( get_post_type(), 'author' ) ) {
+	function twenty_twenty_one_posted_by()
+	{
+		if (! get_the_author_meta('description') && post_type_supports(get_post_type(), 'author')) {
 			echo '<span class="byline">';
 			printf(
 				/* translators: %s: Author name. */
-				esc_html__( 'By %s', 'twentytwentyone' ),
-				'<a href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '" rel="author">' . esc_html( get_the_author() ) . '</a>'
+				esc_html__('By %s', 'twentytwentyone'),
+				'<a href="' . esc_url(get_author_posts_url(get_the_author_meta('ID'))) . '" rel="author">' . esc_html(get_the_author()) . '</a>'
 			);
 			echo '</span>';
 		}
 	}
 }
 
-if ( ! function_exists( 'twenty_twenty_one_entry_meta_footer' ) ) {
+if (! function_exists('twenty_twenty_one_entry_meta_footer')) {
 	/**
 	 * Prints HTML with meta information for the categories, tags and comments.
 	 * Footer entry meta is displayed differently in archives and single posts.
@@ -61,23 +64,24 @@ if ( ! function_exists( 'twenty_twenty_one_entry_meta_footer' ) ) {
 	 *
 	 * @return void
 	 */
-	function twenty_twenty_one_entry_meta_footer() {
+	function twenty_twenty_one_entry_meta_footer()
+	{
 
 		// Early exit if not a post.
-		if ( 'post' !== get_post_type() ) {
+		if ('post' !== get_post_type()) {
 			return;
 		}
 
 		// Hide meta information on pages.
-		if ( ! is_single() ) {
+		if (! is_single()) {
 
-			if ( is_sticky() ) {
-				echo '<p>' . esc_html_x( 'Featured post', 'Label for sticky posts', 'twentytwentyone' ) . '</p>';
+			if (is_sticky()) {
+				echo '<p>' . esc_html_x('Featured post', 'Label for sticky posts', 'twentytwentyone') . '</p>';
 			}
 
 			$post_format = get_post_format();
-			if ( 'aside' === $post_format || 'status' === $post_format ) {
-				echo '<p><a href="' . esc_url( get_permalink() ) . '">' . twenty_twenty_one_continue_reading_text() . '</a></p>'; // phpcs:ignore WordPress.Security.EscapeOutput
+			if ('aside' === $post_format || 'status' === $post_format) {
+				echo '<p><a href="' . esc_url(get_permalink()) . '">' . twenty_twenty_one_continue_reading_text() . '</a></p>'; // phpcs:ignore WordPress.Security.EscapeOutput
 			}
 
 			// Posted on.
@@ -87,31 +91,31 @@ if ( ! function_exists( 'twenty_twenty_one_entry_meta_footer' ) ) {
 			edit_post_link(
 				sprintf(
 					/* translators: %s: Post title. Only visible to screen readers. */
-					esc_html__( 'Edit %s', 'twentytwentyone' ),
+					esc_html__('Edit %s', 'twentytwentyone'),
 					'<span class="screen-reader-text">' . get_the_title() . '</span>'
 				),
 				'<span class="edit-link">',
 				'</span><br>'
 			);
 
-			if ( has_category() || has_tag() ) {
+			if (has_category() || has_tag()) {
 
 				echo '<div class="post-taxonomies">';
 
-				$categories_list = get_the_category_list( wp_get_list_item_separator() );
-				if ( $categories_list ) {
+				$categories_list = get_the_category_list(wp_get_list_item_separator());
+				if ($categories_list) {
 					printf(
 						/* translators: %s: List of categories. */
-						'<span class="cat-links">' . esc_html__( 'Categorized as %s', 'twentytwentyone' ) . ' </span>',
+						'<span class="cat-links">' . esc_html__('Categorized as %s', 'twentytwentyone') . ' </span>',
 						$categories_list // phpcs:ignore WordPress.Security.EscapeOutput
 					);
 				}
 
-				$tags_list = get_the_tag_list( '', wp_get_list_item_separator() );
-				if ( $tags_list ) {
+				$tags_list = get_the_tag_list('', wp_get_list_item_separator());
+				if ($tags_list) {
 					printf(
 						/* translators: %s: List of tags. */
-						'<span class="tags-links">' . esc_html__( 'Tagged %s', 'twentytwentyone' ) . '</span>',
+						'<span class="tags-links">' . esc_html__('Tagged %s', 'twentytwentyone') . '</span>',
 						$tags_list // phpcs:ignore WordPress.Security.EscapeOutput
 					);
 				}
@@ -128,7 +132,7 @@ if ( ! function_exists( 'twenty_twenty_one_entry_meta_footer' ) ) {
 			edit_post_link(
 				sprintf(
 					/* translators: %s: Post title. Only visible to screen readers. */
-					esc_html__( 'Edit %s', 'twentytwentyone' ),
+					esc_html__('Edit %s', 'twentytwentyone'),
 					'<span class="screen-reader-text">' . get_the_title() . '</span>'
 				),
 				'<span class="edit-link">',
@@ -136,24 +140,24 @@ if ( ! function_exists( 'twenty_twenty_one_entry_meta_footer' ) ) {
 			);
 			echo '</div>';
 
-			if ( has_category() || has_tag() ) {
+			if (has_category() || has_tag()) {
 
 				echo '<div class="post-taxonomies">';
 
-				$categories_list = get_the_category_list( wp_get_list_item_separator() );
-				if ( $categories_list ) {
+				$categories_list = get_the_category_list(wp_get_list_item_separator());
+				if ($categories_list) {
 					printf(
 						/* translators: %s: List of categories. */
-						'<span class="cat-links">' . esc_html__( 'Categorized as %s', 'twentytwentyone' ) . ' </span>',
+						'<span class="cat-links">' . esc_html__('Categorized as %s', 'twentytwentyone') . ' </span>',
 						$categories_list // phpcs:ignore WordPress.Security.EscapeOutput
 					);
 				}
 
-				$tags_list = get_the_tag_list( '', wp_get_list_item_separator() );
-				if ( $tags_list ) {
+				$tags_list = get_the_tag_list('', wp_get_list_item_separator());
+				if ($tags_list) {
 					printf(
 						/* translators: %s: List of tags. */
-						'<span class="tags-links">' . esc_html__( 'Tagged %s', 'twentytwentyone' ) . '</span>',
+						'<span class="tags-links">' . esc_html__('Tagged %s', 'twentytwentyone') . '</span>',
 						$tags_list // phpcs:ignore WordPress.Security.EscapeOutput
 					);
 				}
@@ -163,7 +167,7 @@ if ( ! function_exists( 'twenty_twenty_one_entry_meta_footer' ) ) {
 	}
 }
 
-if ( ! function_exists( 'twenty_twenty_one_post_thumbnail' ) ) {
+if (! function_exists('twenty_twenty_one_post_thumbnail')) {
 	/**
 	 * Displays an optional post thumbnail.
 	 *
@@ -174,21 +178,22 @@ if ( ! function_exists( 'twenty_twenty_one_post_thumbnail' ) ) {
 	 *
 	 * @return void
 	 */
-	function twenty_twenty_one_post_thumbnail() {
-		if ( ! twenty_twenty_one_can_show_post_thumbnail() ) {
+	function twenty_twenty_one_post_thumbnail()
+	{
+		if (! twenty_twenty_one_can_show_post_thumbnail()) {
 			return;
 		}
-		?>
+?>
 
-		<?php if ( is_singular() ) : ?>
+		<?php if (is_singular()) : ?>
 
 			<figure class="post-thumbnail">
 				<?php
 				// Lazy-loading attributes should be skipped for thumbnails since they are immediately in the viewport.
-				the_post_thumbnail( 'post-thumbnail', array( 'loading' => false ) );
+				the_post_thumbnail('post-thumbnail', array('loading' => false));
 				?>
-				<?php if ( wp_get_attachment_caption( get_post_thumbnail_id() ) ) : ?>
-					<figcaption class="wp-caption-text"><?php echo wp_kses_post( wp_get_attachment_caption( get_post_thumbnail_id() ) ); ?></figcaption>
+				<?php if (wp_get_attachment_caption(get_post_thumbnail_id())) : ?>
+					<figcaption class="wp-caption-text"><?php echo wp_kses_post(wp_get_attachment_caption(get_post_thumbnail_id())); ?></figcaption>
 				<?php endif; ?>
 			</figure><!-- .post-thumbnail -->
 
@@ -196,36 +201,39 @@ if ( ! function_exists( 'twenty_twenty_one_post_thumbnail' ) ) {
 
 			<figure class="post-thumbnail">
 				<a class="post-thumbnail-inner alignwide" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
-					<?php the_post_thumbnail( 'post-thumbnail' ); ?>
+					<?php the_post_thumbnail('post-thumbnail'); ?>
 				</a>
-				<?php if ( wp_get_attachment_caption( get_post_thumbnail_id() ) ) : ?>
-					<figcaption class="wp-caption-text"><?php echo wp_kses_post( wp_get_attachment_caption( get_post_thumbnail_id() ) ); ?></figcaption>
+				<?php if (wp_get_attachment_caption(get_post_thumbnail_id())) : ?>
+					<figcaption class="wp-caption-text"><?php echo wp_kses_post(wp_get_attachment_caption(get_post_thumbnail_id())); ?></figcaption>
 				<?php endif; ?>
 			</figure>
 
 		<?php endif; ?>
-		<?php
+<?php
 	}
 }
 
-if ( ! function_exists( 'twenty_twenty_one_the_posts_navigation' ) ) {
+
+if (!function_exists('wpexcellent\the_posts_navigation')) {
 	/**
-	 * Print the next and previous posts navigation.
-	 *
-	 * @since Twenty Twenty-One 1.0
+	 * Print the next and previous posts navigation with custom modifications.
+	 * Modify output adding ASCII arrows and class.
 	 *
 	 * @return void
 	 */
-	function twenty_twenty_one_the_posts_navigation() {
+	function the_posts_navigation()
+	{
+		ob_start();
+
 		the_posts_pagination(
 			array(
-				'before_page_number' => esc_html__( 'Page', 'twentytwentyone' ) . ' ',
+				'before_page_number' => esc_html__('Page', 'wpexcellent') . ' ',
 				'mid_size'           => 0,
 				'prev_text'          => sprintf(
 					'%s <span class="nav-prev-text">%s</span>',
-					is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' ),
+					is_rtl() ? '→' : '←', // ASCII arrows for RTL and LTR
 					wp_kses(
-						__( 'Newer <span class="nav-short">posts</span>', 'twentytwentyone' ),
+						__('Newer <span class="nav-short">posts</span>', 'wpexcellent'),
 						array(
 							'span' => array(
 								'class' => array(),
@@ -236,16 +244,22 @@ if ( ! function_exists( 'twenty_twenty_one_the_posts_navigation' ) ) {
 				'next_text'          => sprintf(
 					'<span class="nav-next-text">%s</span> %s',
 					wp_kses(
-						__( 'Older <span class="nav-short">posts</span>', 'twentytwentyone' ),
+						__('Older <span class="nav-short">posts</span>', 'wpexcellent'),
 						array(
 							'span' => array(
 								'class' => array(),
 							),
 						)
 					),
-					is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' )
+					is_rtl() ? '←' : '→' // ASCII arrows for RTL and LTR
 				),
 			)
 		);
+
+		$pagination = ob_get_clean();
+
+		$pagination = str_replace('<div class="nav-links"', '<div class="cluster"', $pagination);
+
+		echo $pagination;
 	}
 }
